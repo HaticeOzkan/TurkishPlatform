ec6aa5d81570d1f41e86a11959be1e4825729e32
HaticeOzkan <46075358+HaticeOzkan@users.noreply.github.com>
2019-02-05 14:50:04 +0300

    Areas

---
+++ TurkishPlatform/Areas/Areas/AreasAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace TurkishPlatform.Areas.Areas
+{
+    public class AreasAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Areas";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Areas_default",
+                "Areas/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
