946261ad258177f9b5e0492e877736502758bc30
Dicle <Dicle@DICLE>
2019-02-06 14:57:37 +0300

    activity update

--- TurkishPlatform/Models/Activity.cs
+++ TurkishPlatform/Models/Activity.cs
@@ -19,14 +19,22 @@ namespace TurkishPlatform.Models
 		[MaxLength(2000)]
 		public string Content { get; set; }
         public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime FinishTime { get; set; }
+        public string Address { get; set; }
         public TimeSpan Remaining { get; set; }
 		public Country Country { get; set; }
 		[ForeignKey("Country")]
 		public int CountryId { get; set; }
-
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public bool Permission { get; set; }
         public Activity()
         {
             Remaining = Date - DateTime.Now;
+            Permission = false;
         }
+       
     }
 }
