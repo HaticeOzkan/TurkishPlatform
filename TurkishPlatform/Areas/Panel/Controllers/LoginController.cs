1e2bd62084de5d1a30253666d78f44754b054ccd
HaticeOzkan <46075358+HaticeOzkan@users.noreply.github.com>
2019-02-06 15:00:41 +0300

    Sifre İslemleri

--- TurkishPlatform/Areas/Panel/Controllers/LoginController.cs
+++ TurkishPlatform/Areas/Panel/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,12 +10,43 @@ namespace TurkishPlatform.Areas.Panel.Controllers
     public class LoginController : Controller
     {
         // GET: Panel/Login
-        public ActionResult Index(string Email,string Password)
+        public ActionResult Index(string UserName,string Password)
         {
-            if(Email== "dalkranberna@gmail.com" && Password == "32078")
+            bool IsTrue = true;
+            ViewBag.Message = "Sorry. Your password was incorrect. Please double-check your password.";
+            if (UserName == "Admin" && Password == "32078")
             {
                 return RedirectToAction("Index", "Countries");
             }
+            else
+                IsTrue = false;
+            return View(IsTrue);
+        }
+      
+        public ActionResult ForgotPassword(string TextEmail)
+        {
+            MailMessage ePosta = new MailMessage();
+            ePosta.From = new MailAddress("Ornek@gmail.com");
+
+            ePosta.To.Add(TextEmail);
+            ePosta.Subject = "TurkishPlatform Renewal Password";
+            ePosta.Body = "<a href="+"'"+"/ Panel / Login / RenewalPassword"+"'"+">Renewal Password</a>";
+
+            SmtpClient smtp = new SmtpClient();
+            #region HesapBilgileri
+            smtp.Credentials = new System.Net.NetworkCredential("ourturkishplatform@gmail.com", "3d1sWissen");
+            #endregion
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            smtp.Send(ePosta);
+            //Girilen email alınacak bu email e mail gönderilcek mailde şifre yenileme linki olacak onu basınca şifre yenileme sayfası gelecek
+            return View();
+        }
+        //şifre yenileme sayfasına yönlendirilecek
+        public ActionResult RenewalPassword()
+        {
+
             return View();
         }
     }
---
+++ TurkishPlatform/Models/AdminProfil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TurkishPlatform.Models
+{
+    public class AdminProfil
+    {
+        [Key]
+        public int InfoID { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string MailAdress { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public int Password { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+    }
+}
