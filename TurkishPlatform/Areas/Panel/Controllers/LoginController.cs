a3701827fbd414b523b94552329aac0d6cea78d4
HaticeOzkan <46075358+HaticeOzkan@users.noreply.github.com>
2019-02-07 14:59:23 +0300

    Çoklu Admin Durumları

--- TurkishPlatform/Areas/Panel/Controllers/LoginController.cs
+++ TurkishPlatform/Areas/Panel/Controllers/LoginController.cs
@@ -4,24 +4,40 @@ using System.Linq;
 using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using TurkishPlatform.Areas.Panel.Models;
 using TurkishPlatform.Models;
 
 namespace TurkishPlatform.Areas.Panel.Controllers
 {
     public class LoginController : Controller
     {
-
+        AdminView adminView = new AdminView();//birden çok admin olabilir
+        PlatformContext Db = new PlatformContext();
         // GET: Panel/Login
         //Burası yöneticinin Giriş yaptıgı bölüm aşağıda şifremi unutuum kısmı var 
         [HttpPost]
         [ValidateAntiForgeryToken]//Güvenlik için bunu koymazsak postta javascript atakları yapıp verilerimize ulaşılabilir
-        public ActionResult Index(string UserName,string Password)
+        public ActionResult Index(string UserName,int Password)
         {
+           
             bool? IsTrue = true;
-            ViewBag.Message = "Sorry. Your password was incorrect. Please double-check your password.";
-            if (UserName == "Admin" && Password == "32078")
+          
+            List<AdminProfil> ListProfile = Db.AdminProfils.ToList();
+            foreach (var item in ListProfile)
+            {
+                if (UserName==item.Name)
+                {
+                    adminView.Name = item.Name;                   
+                    adminView.Password = item.Password;
+                    adminView.Email = item.MailAdress;
+                   
+                }
+            }
+            ViewBag.Message = "Sorry. Your password or name was incorrect. Please double-check your password.";
+            if (UserName == adminView.Name && Password == adminView.Password)
             {
                 return RedirectToAction("Index", "Countries");
+
             }
             else
                 IsTrue = false;//eger şifresini yanlış girmişse
@@ -43,7 +59,7 @@ namespace TurkishPlatform.Areas.Panel.Controllers
 
             ePosta.To.Add(TextEmail);
             ePosta.Subject = "TurkishPlatform Renewal Password";
-            ePosta.Body = "<a href="+"'"+"/ Panel / Login / RenewalPassword"+"'"+">Renewal Password</a>";
+            ePosta.Body = "http://localhost:59574/Panel/Login/RenewalPassword";
 
             SmtpClient smtp = new SmtpClient();
             #region HesapBilgileri
@@ -85,5 +101,6 @@ namespace TurkishPlatform.Areas.Panel.Controllers
 
             return View();
         }
+
     }
 }
