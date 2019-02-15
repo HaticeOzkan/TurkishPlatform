0992f67a9a1908f91d8b8e5322e0064d10704aed
HaticeOzkan <46075358+HaticeOzkan@users.noreply.github.com>
2019-02-15 15:03:07 +0300

    login2

---
+++ TurkishPlatform/Models/FileViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TurkishPlatform.Models
+{
+    public class File
+    {
+        public string name { get; set; }
+        public int size { get; set; }
+        public string url { get; set; }
+        public string thumbnailUrl { get; set; }
+        public string deleteUrl { get; set; }
+        public string deleteType { get; set; }
+
+    }
+    public class FileList
+    {
+        public List<File> files = new List<File>();
+    }
+}
