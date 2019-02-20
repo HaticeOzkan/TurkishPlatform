a9baa85ac3babdbefe4468f44b401e520afde4f8
Dicle <Dicle@DICLE>
2019-02-20 23:46:23 +0300

    commentdelete

--- TurkishPlatform/Controllers/TourismTravelController.cs
+++ TurkishPlatform/Controllers/TourismTravelController.cs
@@ -12,22 +12,56 @@ namespace TurkishPlatform.Controllers
         // GET: TourismTravel
         public ActionResult Index()
         {
-            PlatformContext db = new PlatformContext(); 
-           ViewBag.AllTourism = db.Tourisms.ToList();
+            PlatformContext db = new PlatformContext();
+            ViewBag.AllTourism = db.Tourisms.ToList();
 
             return View(db.Tourisms.ToList());
         }
-        public ActionResult TourismTravelDetails(LocationComment d, int id ,int UserId,int locationId)
-        { 
-            int a = (int)Session["EnterID"];
-            
+
+        [HttpGet]
+        public ActionResult TourismTravelDetails(int id, int? UserId, int locationId)
+        {
             PlatformContext db = new PlatformContext();
+            ViewBag.Name = UserId;
+            ViewBag.c = (Session["EnterID"] == null); 
             ViewBag.user = db.Users.Find(id);
             ViewBag.Comment = db.LocationComments.ToList();
             ViewBag.Tourism = db.Tourisms.Where(x => x.CountryId == id);
-            Tourism countries =( from c in db.Tourisms where c.CountryId==id select c).FirstOrDefault();
-             ViewBag.Comments = db.LocationComments.Where(x => x.LocationId == locationId & x.UserId == UserId);
+            Tourism countries = (from c in db.Tourisms where c.CountryId == id select c).FirstOrDefault();
+            ViewBag.Comments = db.LocationComments.Where(x => x.LocationId == locationId & x.UserId == UserId);
             return View(countries);
+        }
+
+
+        [HttpPost]
+        public ActionResult TourismTravelDetails(string comment, int id, int? UserId, int locationId)
+        {
+            PlatformContext db = new PlatformContext();
+            ViewBag.c = (Session["EnterID"] == null);
+            ViewBag.Name = UserId;
+                //int a = (int)Session["EnterID"];
+                if (!string.IsNullOrEmpty(comment))
+            {
+                LocationComment lc = new LocationComment();
+                lc.Content = comment;
+                lc.LocationId = (int)locationId;
+                lc.UserId = (int)UserId;
+                db.LocationComments.Add(lc);
+                db.SaveChanges();
+            } 
+
+            ViewBag.user = db.Users.Find(id);
+            ViewBag.Comment = db.LocationComments.ToList();
+            ViewBag.Tourism = db.Tourisms.Where(x => x.CountryId == id);
+            Tourism countries = (from c in db.Tourisms where c.CountryId == id select c).FirstOrDefault();
+            ViewBag.Comments = db.LocationComments.Where(x => x.LocationId == locationId & x.UserId == UserId);
+            return View(countries);
+        }
+
+
+        public JsonResult DeleteComment(int deleteid)
+        {
+            return Json(2);
         }
     }
 }
