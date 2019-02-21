d0c76644aafbe96d6c0bbe3b943191486f5f61a5
Berna <Asus@DESKTOP-U25LG74>
2019-02-21 22:02:12 +0300

    restaurant-comment

--- TurkishPlatform/Controllers/RestaurantController.cs
+++ TurkishPlatform/Controllers/RestaurantController.cs
@@ -7,47 +7,59 @@ using TurkishPlatform.Models;
 
 namespace TurkishPlatform.Controllers
 {
-	public class RestaurantController : Controller
-	{
-		// GET: Restaurant
-		PlatformContext db = new PlatformContext();
-		public ActionResult Index()
-		{
-			RestaurantViewModel data = new RestaurantViewModel();
-			data.RestaurantId = db.Restaurants.Select(x => x.RestaurantId).FirstOrDefault();
-			data.RestaurantName = db.Restaurants.Select(x => x.RestaurantName).FirstOrDefault();
-			data.Address = db.Restaurants.Select(x => x.Address).FirstOrDefault();
-			data.CoverImageURL = db.Restaurants.Select(x => x.CoverImageURL).FirstOrDefault();
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        PlatformContext db = new PlatformContext();
+        public ActionResult Index()
+        {
+            RestaurantViewModel data = new RestaurantViewModel();
+            data.RestaurantId = db.Restaurants.Select(x => x.RestaurantId).FirstOrDefault();
+            data.RestaurantName = db.Restaurants.Select(x => x.RestaurantName).FirstOrDefault();
+            data.Address = db.Restaurants.Select(x => x.Address).FirstOrDefault();
+            data.CoverImageURL = db.Restaurants.Select(x => x.CoverImageURL).FirstOrDefault();
 
-			ViewBag.AllRestaurant = db.Restaurants.ToList();
+            ViewBag.AllRestaurant = db.Restaurants.ToList();
 
-			return View(data);
-		}
+            return View(data);
+        }
 
 
-		public ActionResult RestaurantDetail(int id)
-		{
-			Restaurant r = db.Restaurants.Include("Country").FirstOrDefault(x => x.RestaurantId == id);
-			ViewBag.RestaurantId = r.RestaurantId;
-			ViewBag.Comment = db.RestaurantComments.Where(x => x.RestaurantId == id).ToList();
+        public ActionResult RestaurantDetail(int id)
+        {
+            Restaurant r = db.Restaurants.Include("Country").FirstOrDefault(x => x.RestaurantId == id);
+            ViewBag.RestaurantId = r.RestaurantId;
+            ViewBag.Comment = db.RestaurantComments.Where(x => x.RestaurantId == id).ToList();
 
-			return View(r);
-		}
+            return View(r);
+        }
 
-		[HttpPost]
-		public ActionResult RestaurantDetail(string Content, int id)
-		{
-			
-			RestaurantComment rComment = new RestaurantComment();
-		
-			rComment.Content = Content;
-			rComment.RestaurantId = id;
-			rComment.User.UserId = Convert.ToInt32(Session["EnterID"]);
+        [HttpPost]
+        public ActionResult RestaurantDetail(string Content, int id)
+        {
 
-			db.RestaurantComments.Add(rComment);
-			db.SaveChanges();
+            RestaurantComment rComment = new RestaurantComment();
 
-			return View(rComment);
-		}
-	}
+            rComment.Content = Content;
+            rComment.RestaurantId = id;
+            rComment.UserId = Convert.ToInt32(Session["EnterID"]);
+
+            db.RestaurantComments.Add(rComment);
+            db.SaveChanges();
+
+            Restaurant rest = (from r in db.Restaurants where r.RestaurantId == id select r).FirstOrDefault();
+            ViewBag.Comment = db.RestaurantComments.Where(x => x.RestaurantId == id).ToList();
+
+            return View(rest);
+        }
+
+        public JsonResult DeleteComment(int deleteid)
+        {
+            RestaurantComment delComment = db.RestaurantComments.Find(deleteid);
+            db.RestaurantComments.Remove(delComment);
+            db.SaveChanges();
+
+            return Json(2);
+        }
+    }
 }
